77f5474422912b7793264b058269a11c85db48ec
cybergrubert <mateusag05@gmail.com>
2025-11-17 22:21:25 -0300

    finalizado

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
-console.log("Calculadora de Partidas Rankeadas")
-console.log("vai mengão")
+function saldo(win, lose) {
+  return win - lose;
+}
+let resultado = saldo(150, 30);
+if (resultado < 10) {
+  console.log("O Herói tem saldo de " + resultado + " vitórias e está no nível FERRO");
+} else if (resultado >= 11 && resultado <= 20) {
+  console.log("O Herói tem saldo de " + resultado + " vitórias e está no nível BRONZE");
+} else if (resultado >= 21 && resultado <= 50) {
+  console.log("O Herói tem saldo de " + resultado + " vitórias e está no nível PRATA");
+} else if (resultado >= 51 && resultado <= 80) {
+  console.log("O Herói tem saldo de " + resultado + " vitórias e está no nível OURO");
+} else if (resultado >= 81 && resultado <= 90) {
+  console.log("O Herói tem saldo de " + resultado + " vitórias e está no nível DIAMANTE");
+} else if (resultado >= 91 && resultado <= 100) {
+  console.log("O Herói tem saldo de " + resultado + " vitórias e está no nível LENDÁRIO");
+} else if (resultado > 100) {
+  console.log("O Herói tem saldo de " + resultado + " vitórias e está no nível IMORTAL");
+}
